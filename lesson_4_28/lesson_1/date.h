46e69953ec12970d5b17159e9efd478bdf12235e
Darling1116 <1154883599@qq.com>
2022-04-28 13:16:00 +0800

    "Date"

---
+++ lesson_4_28/lesson_1/date.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+class Date
+{
+public:
+	Date(int year = 2020, int month = 1, int day = 1);  //声明时给缺省值
+
+	void Print();
+
+	int getMonthDay(int year, int month);
+
+	Date& operator+=(int day);
+	Date operator+(int day);
+
+	Date& operator-=(int day);
+	Date operator-(int day);
+
+	//后置++/--:为了区分前置的，给其参数列表加了一个参数
+	Date operator++(int);
+	Date operator--(int);
+
+	//前置++/--
+	Date& operator++();
+	Date& operator--();
+
+	bool operator>(const Date& d);
+	bool operator==(const Date& d);
+	bool operator>=(const Date& d);
+	bool operator<(const Date& d);
+	bool operator<=(const Date& d);
+	bool operator!=(const Date& d);
+
+
+	//两个日期相减
+	int operator-(const Date& d);
+
+
+
+
+	~Date();
+
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+
+
---
+++ lesson_4_28/lesson_1/test.cpp
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "date.h"
+
+int main(){
+	Date d1;
+	d1.Print();
+
+	Date d2(2020, 12, 3);
+	d2.Print();
+
+	//d2 += 12;
+	//d2.Print();
+
+	//(d2 + 12).Print();
+	//d2.Print();
+
+	//d2 -= 22;
+	//d2.Print();
+
+	//(d2 - 12).Print();
+	//d2.Print();
+
+	//d2++.Print();
+	//d2.Print();
+
+	//++d1;
+	//d1.Print();
+
+
+	int count1 = d1 - d2;
+	cout << count1 << endl;
+
+	int count2 = d2 - d1;
+	cout << count2 << endl;
+
+	//d2--.Print();
+	//d2.Print();
+
+	//bool ret1 = d2 > d1;
+	//cout << ret1 << endl;
+
+	//bool ret2 = d2 == d1;
+	//cout << ret2 << endl;
+
+	//bool ret3 = d2 <= d1;
+	//cout << ret3 << endl;
+
+	//cout << d2.getMonthDay(2020, 2) << endl;
+	system("pause");
+	return 0;
+
+}
+
