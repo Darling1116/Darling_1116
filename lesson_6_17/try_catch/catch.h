f55284c1b5d45a844622450b65f559f257ba3973
Darling1116 <1154883599@qq.com>
2022-06-20 22:20:55 +0800

    "try_catch"

--- lesson_6_17/try_catch/catch.h
+++ lesson_6_17/try_catch/catch.h
@@ -29,3 +29,39 @@ void test_2(){
 		cout << "除0错误..."  << endl;
 	}
 }
+
+//异常的重新抛出
+double Division(int a, int b) {
+	// 当b == 0时抛出异常
+	if (b == 0){
+		throw "Division by zero condition!";
+	}
+	return (double)a / (double)b;
+}
+
+void Func(){
+	// 如果发生除0错误抛出异常，下面的array没有得到释放，会造成内存泄漏
+	// 所以这里捕获异常后并不处理异常，异常还是交给外面处理，这里捕获了再重新抛出去
+	int* array = new int[10];
+	try {
+		int len, time;
+		cin >> len >> time;
+		cout << Division(len, time) << endl;
+	}
+	catch (...)
+	{
+		cout << "delete []" << array << endl;
+		delete[] array;
+		throw;
+	}
+	cout << "delete []" << array << endl;
+	delete[] array;
+}
+void test_3(){
+	try{
+		Func();
+	}
+	catch (const char* errmsg){
+		cout << errmsg << endl;
+	}
+}
--- lesson_6_17/try_catch/test.cpp
+++ lesson_6_17/try_catch/test.cpp
@@ -1,10 +1,12 @@
 #include "catch.h"
-
+#include "exception.h"
 
 int main(){
 
 	//test_1();
-	test_2();
+	//test_2();
+	//test_3();
+	test_e1();
 	system("pause");
 	return 0;
 }
