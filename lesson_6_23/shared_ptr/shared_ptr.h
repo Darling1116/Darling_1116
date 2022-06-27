d6c1bc95a9b1536f1ff4d864e5949d306bc0a021
Darling1116 <1154883599@qq.com>
2022-06-27 22:00:40 +0800

    "shared_ptr"

--- lesson_6_23/shared_ptr/shared_ptr.h
+++ lesson_6_23/shared_ptr/shared_ptr.h
@@ -1,23 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <mutex>
+#include <thread>
 
 using namespace std;
 
 namespace GXR{
-	class Date{
+	struct Date{
 	public:
-		Date() { 
-			cout << "Date()" << endl; 
+		Date(int year = 2022, int month = 6, int day = 27)
+			:_year(year),
+			_month(month),
+			_day(day){ 
+			//cout << "Date()" << endl; 
 		}
 
 		~Date(){ 
-			cout << "~Date()" << endl; 
+			//cout << "~Date()" << endl; 
 		}
 		int _year;
 		int _month;
 		int _day;
 	};
 
+	//让智能指针去管理Date对象
 
 	template <class T>
 	class auto_ptr{
@@ -109,5 +115,150 @@ namespace GXR{
 		ap2->_month = 2012;
 		//ap1 = ap2;
 		//unique_ptr<Date> ap3(ap1);
+	}
+
+
+	//引入了引用计数，让多个智能指针对象管理同一块资源
+	template <class T>
+	class shared_ptr{
+	public:
+		shared_ptr(T* ptr = nullptr)
+			:_ptr(ptr),
+			_count(new int(1)),
+			_pmutex(new mutex){
+			cout << "shared_ptr..." << _ptr << endl;
+		}
+
+		T& operator*(){
+			return *_ptr;
+		}
+
+		T* operator->(){
+			return _ptr;
+		}
+
+		int& get_count(){
+			return *_count;
+		}
+
+		T* get_ptr(){
+			return _ptr;
+		}
+
+		//拷贝构造时，引用计数加1
+		shared_ptr(shared_ptr<T>& sp)
+			:_ptr(sp._ptr),
+			_count(sp._count),
+			_pmutex(sp._pmutex){
+			add_ref_count();
+			cout << _count << ": " << *_count << endl;
+			cout << "shared_ptr..." << _ptr << endl;
+		}
+
+		//赋值重载时，引用计数加1
+		shared_ptr<T>& operator=(shared_ptr<T>& sp){
+			if (_ptr != sp._ptr){  
+				//先处理this以前的资源
+				/*if (--(*_count) == 0){
+					delete _ptr;
+					delete _count;
+				}*/
+				release();
+				//赋值
+				_ptr = sp._ptr;
+				_count = sp._count;
+				_pmutex = sp._pmutex;
+				//++(*_count);
+				add_ref_count();
+			}
+			cout << "operator=..." << _ptr << endl;
+			return *this;
+		}
+		
+		~shared_ptr(){
+			//if (--(*_count) == 0){
+			//	cout << "delete..." << _ptr << endl;
+			//	delete _ptr;
+			//	delete _count;
+			//}
+			release();
+		}
+
+	private:
+		void add_ref_count(){
+			_pmutex->lock();
+			++(*_count);
+			_pmutex->unlock();
+		}
+
+		void release(){  //释放以前的资源：锁也是资源，也要释放
+			bool deleteflag = false;
+
+			_pmutex->lock();
+			if (--(*_count) == 0){
+				cout << "delete..." << _ptr << endl;
+				delete _ptr;
+				delete _count;
+				deleteflag = true;
+			}
+			_pmutex->unlock();
+
+			if (deleteflag == true){
+				delete _pmutex;
+				cout << "delete pmutex..." << _pmutex << endl;
+			}
+		}
+
+	private:
+		T* _ptr;
+		int* _count;  
+		mutex* _pmutex;
+	};
+
+	void test_shared_ptr_1(){
+		shared_ptr<Date> sp1(new Date);
+		shared_ptr<Date> sp2(new Date(2022, 12, 12));
+		//cout << sp2->_month << endl;
+		//shared_ptr<Date> copy(sp1);
+		sp1 = sp2;	
+	}
+
+	void test_shared_ptr_2(){
+		shared_ptr<Date> sp1(new Date);
+		//shared_ptr<Date> sp2(new Date(2022, 12, 12));
+		shared_ptr<Date> sp3(sp1);
+		sp3->_year = 2000;
+		cout << sp1.get_count() << endl;
+		cout << sp3.get_count() << endl;
+	}
+
+	//测试多线程安全问题
+
+	void shared_func(shared_ptr<Date>& sp, size_t n){
+		cout << sp.get_ptr() << endl;
+		for (size_t i = 0; i < n; i++){
+			//调用其拷贝构造函数，会产生_count++的操作
+			shared_ptr<Date> copy(sp);
+			copy->_year++;
+			copy->_month++;
+			copy->_day++;
+		}
+	}
+
+	void test_shared_ptr_3(){
+		shared_ptr<Date> sp(new Date(2000, 1, 1));
+		cout << sp.get_ptr() << endl;
+		cout << sp->_year << endl;
+
+		size_t n = 1000;
+		//让线程t1、t2执行相同的逻辑
+		thread t1(shared_func, sp, n);
+		thread t2(shared_func, sp, n);
+		t1.join();
+		t2.join();
+		
+		cout << sp->_year << endl;
+		
+		
 	}
 }
--- lesson_6_23/shared_ptr/test.cpp
+++ lesson_6_23/shared_ptr/test.cpp
@@ -5,7 +5,10 @@ using namespace GXR;
 int main(){
 
 	//test_auto_ptr();
-	test_unique_ptr();
+	//test_unique_ptr();
+	//test_shared_ptr_1();
+	//test_shared_ptr_2();
+	test_shared_ptr_3();
 	system("pause");
 	return 0;
 }
