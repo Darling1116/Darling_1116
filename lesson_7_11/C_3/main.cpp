ccce61a7c3451f1615d2fb5448986e2857152d45
Darling1116 <1154883599@qq.com>
2022-07-11 23:40:25 +0800

    "C_3"

---
+++ lesson_7_11/C_3/main.cpp
@@ -0,0 +1,9 @@
+#include "test_1.h"
+
+int main(){
+	//test1();
+	//test2();
+	test3_1();
+	system("pause");
+	return 0;
+}
---
+++ lesson_7_11/C_3/test_1.h
@@ -0,0 +1,77 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+void test1(){
+	char str1[] = "hello bit.";
+	char str2[] = "hello bit.";
+	char *str3 = "hello bit.";
+	char *str4 = "hello bit.";
+
+	if (str1 == str2)
+		printf("str1 and str2 are same\n");
+	else
+		printf("str1 and str2 are not same\n");
+
+	if (str3 == str3)
+		printf("str3 and str4 are same\n");
+	else
+		printf("str3 and str4 are not same\n");
+
+}
+
+
+int add(int a, int b) {
+	return a + b;
+}
+int sub(int a, int b) {
+	return a - b;
+}
+int mul(int a, int b) {
+	return a*b;
+}
+int divsion(int a, int b) {
+	return a / b;
+}
+
+void test2(){
+	int x, y;
+	int input = 1;
+	int ret = 0;
+	// 函数指针数组
+	int(*p[5])(int x, int y) = { 0, add, sub, mul, divsion }; //转移表
+	while (input){
+		printf("*************************\n");
+		printf(" 1:add             2:sub \n");
+		printf(" 3:mul             4:div \n");
+		printf("*************************\n");
+		printf("请选择：");
+		scanf("%d", &input);
+		if ((input <= 4 && input >= 1))
+		{
+			printf("输入操作数：");
+			scanf("%d %d", &x, &y);
+			ret = (*p[input])(x, y);
+		}
+		else
+			printf("输入有误\n");
+		printf("ret = %d\n", ret);
+	}
+}
+
+
+void test3_1(){
+	//一维数组
+	int a[] = { 1, 2, 3, 4 };
+	printf("%d\n", sizeof(a));
+	printf("%d\n", sizeof(a + 0));
+	printf("%d\n", sizeof(*a));
+	printf("%d\n", sizeof(a + 1));
+	printf("%d\n", sizeof(a[1]));
+	printf("%d\n", sizeof(&a));
+	printf("%d\n", sizeof(*&a));
+	printf("%d\n", sizeof(&a + 1));
+	printf("%d\n", sizeof(&a[0]));
+	printf("%d\n", sizeof(&a[0] + 1));
+}
+
