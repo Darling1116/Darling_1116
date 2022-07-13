a4c4bb0ab278d786b74925ec028fbe0f0f614d9d
Darling1116 <1154883599@qq.com>
2022-07-13 10:04:06 +0800

    "C_3"

--- lesson_7_11/C_3/main.cpp
+++ lesson_7_11/C_3/main.cpp
@@ -1,4 +1,5 @@
 #include "test_1.h"
+#include "test_2.h"
 
 int main(){
 	//test1();
@@ -7,7 +8,14 @@ int main(){
 	//test3_2();
 	//test3_3();
 	//test3_4();
-	test3_5();
+	//test3_5();
+
+	//test2_1();
+	//test2_2();
+	//test2_3();
+	//test2_4();
+	test2_5();
+	test2_6();
 	system("pause");
 	return 0;
 }
